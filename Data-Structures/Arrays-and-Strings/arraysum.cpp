fc1aff188157fb9cfb558611000fb99e78305424
Yunus Been Hashem <yunuszoro46@gmail.com>
2026-09-23 00:53:57 +0600

    Add array sum calculation in arraysum.cpp
    Implement a program to calculate the absolute sum of an array.

---
+++ Data-Structures/Arrays-and-Strings/arraysum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int a[n];
+    int sum=0;
+    for(int i=0;i<n;i++) {
+        cin>>a[i];
+        sum =sum +a[i];
+    }
+    cout<<abs(sum);
+}
