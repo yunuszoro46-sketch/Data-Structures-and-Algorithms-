d2b9a55580fa4052fe0c43db1ad26930ee8f53eb
Yunus Been Hashem <yunuszoro46@gmail.com>
2026-09-26 23:20:30 +0600

    Implement matrix addition in matrixaddsub.cpp
    Added matrix addition functionality to combine two matrices.

--- Data-Structures/Arrays-and-Strings/matrixaddsub.cpp
+++ Data-Structures/Arrays-and-Strings/matrixaddsub.cpp
@@ -21,6 +21,13 @@ int main() {
         }
         cout<<endl;
     }
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+            cout<<c[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 
 
 
